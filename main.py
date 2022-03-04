d8fefc53466069680d7e51f2b422a859af0a4e04
Michael Vlamis <michael.vlamis@icloud.com>
2022-03-03 20:36:38 -0500

    add character anim

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+from turtle import window_width
 import pygame
-
-
+import time
 
 pygame.init()
-
 
 display_width = 960
 display_height = 544
@@ -15,55 +14,73 @@ speed = 1
 
 black = (0,0,0)
 white = (255,255,255)
-xPos =  (display_width * 0.45)
-yPos = (display_height * 0.8)
+xPos = (-401)
+yPos = (-401)
 
-x_change = 0
-y_change = 0
+playerSize = 32
+
+counter = 0
 
 clock = pygame.time.Clock()
 crashed = False
-carImg = pygame.image.load('lildude.png')
+playerImg = pygame.image.load('char/Walk/Down/images/Char_walk_down_01.png')
+
+
+downImages = ['char/Walk/Down/images/Char_walk_down_01.png','char/Walk/Down/images/Char_walk_down_01.png','char/Walk/Down/images/Char_walk_down_01.png','char/Walk/Down/images/Char_walk_down_01.png','char/Walk/Down/images/Char_walk_down_01.png','char/Walk/Down/images/Char_walk_down_02.png','char/Walk/Down/images/Char_walk_down_02.png','char/Walk/Down/images/Char_walk_down_02.png','char/Walk/Down/images/Char_walk_down_02.png','char/Walk/Down/images/Char_walk_down_02.png','char/Walk/Down/images/Char_walk_down_03.png','char/Walk/Down/images/Char_walk_down_03.png','char/Walk/Down/images/Char_walk_down_03.png','char/Walk/Down/images/Char_walk_down_03.png','char/Walk/Down/images/Char_walk_down_03.png','char/Walk/Down/images/Char_walk_down_04.png','char/Walk/Down/images/Char_walk_down_04.png','char/Walk/Down/images/Char_walk_down_04.png','char/Walk/Down/images/Char_walk_down_04.png','char/Walk/Down/images/Char_walk_down_05.png','char/Walk/Down/images/Char_walk_down_05.png','char/Walk/Down/images/Char_walk_down_05.png','char/Walk/Down/images/Char_walk_down_05.png','char/Walk/Down/images/Char_walk_down_05.png','char/Walk/Down/images/Char_walk_down_06.png','char/Walk/Down/images/Char_walk_down_06.png','char/Walk/Down/images/Char_walk_down_06.png','char/Walk/Down/images/Char_walk_down_06.png','char/Walk/Down/images/Char_walk_down_06.png']
+leftImages = ['char/Walk/Left/images/Char_walk_left_01.png','char/Walk/Left/images/Char_walk_left_01.png','char/Walk/Left/images/Char_walk_left_01.png','char/Walk/Left/images/Char_walk_left_01.png','char/Walk/Left/images/Char_walk_left_01.png','char/Walk/Left/images/Char_walk_left_02.png','char/Walk/Left/images/Char_walk_left_02.png','char/Walk/Left/images/Char_walk_left_02.png','char/Walk/Left/images/Char_walk_left_02.png','char/Walk/Left/images/Char_walk_left_02.png','char/Walk/Left/images/Char_walk_left_03.png','char/Walk/Left/images/Char_walk_left_03.png','char/Walk/Left/images/Char_walk_left_03.png','char/Walk/Left/images/Char_walk_left_03.png','char/Walk/Left/images/Char_walk_left_03.png','char/Walk/Left/images/Char_walk_left_04.png','char/Walk/Left/images/Char_walk_left_04.png','char/Walk/Left/images/Char_walk_left_04.png','char/Walk/Left/images/Char_walk_left_04.png','char/Walk/Left/images/Char_walk_left_05.png','char/Walk/Left/images/Char_walk_left_05.png','char/Walk/Left/images/Char_walk_left_05.png','char/Walk/Left/images/Char_walk_left_05.png','char/Walk/Left/images/Char_walk_left_05.png','char/Walk/Left/images/Char_walk_left_06.png','char/Walk/Left/images/Char_walk_left_06.png','char/Walk/Left/images/Char_walk_left_06.png','char/Walk/Left/images/Char_walk_left_06.png','char/Walk/Left/images/Char_walk_left_06.png']
+rightImages = ['char/Walk/Right/images/Char_walk_right_01.png','char/Walk/Right/images/Char_walk_right_01.png','char/Walk/Right/images/Char_walk_right_01.png','char/Walk/Right/images/Char_walk_right_01.png','char/Walk/Right/images/Char_walk_right_01.png','char/Walk/Right/images/Char_walk_right_02.png','char/Walk/Right/images/Char_walk_right_02.png','char/Walk/Right/images/Char_walk_right_02.png','char/Walk/Right/images/Char_walk_right_02.png','char/Walk/Right/images/Char_walk_right_02.png','char/Walk/Right/images/Char_walk_right_03.png','char/Walk/Right/images/Char_walk_right_03.png','char/Walk/Right/images/Char_walk_right_03.png','char/Walk/Right/images/Char_walk_right_03.png','char/Walk/Right/images/Char_walk_right_03.png','char/Walk/Right/images/Char_walk_right_04.png','char/Walk/Right/images/Char_walk_right_04.png','char/Walk/Right/images/Char_walk_right_04.png','char/Walk/Right/images/Char_walk_right_04.png','char/Walk/Right/images/Char_walk_right_05.png','char/Walk/Right/images/Char_walk_right_05.png','char/Walk/Right/images/Char_walk_right_05.png','char/Walk/Right/images/Char_walk_right_05.png','char/Walk/Right/images/Char_walk_right_05.png','char/Walk/Right/images/Char_walk_right_06.png','char/Walk/Right/images/Char_walk_right_06.png','char/Walk/Right/images/Char_walk_right_06.png','char/Walk/Right/images/Char_walk_right_06.png','char/Walk/Right/images/Char_walk_right_06.png']
+upImages = ['char/Walk/Up/images/Char_walk_up_01.png','char/Walk/Up/images/Char_walk_up_01.png','char/Walk/Up/images/Char_walk_up_01.png','char/Walk/Up/images/Char_walk_up_01.png','char/Walk/Up/images/Char_walk_up_01.png','char/Walk/Up/images/Char_walk_up_02.png','char/Walk/Up/images/Char_walk_up_02.png','char/Walk/Up/images/Char_walk_up_02.png','char/Walk/Up/images/Char_walk_up_02.png','char/Walk/Up/images/Char_walk_up_02.png','char/Walk/Up/images/Char_walk_up_03.png','char/Walk/Up/images/Char_walk_up_03.png','char/Walk/Up/images/Char_walk_up_03.png','char/Walk/Up/images/Char_walk_up_03.png','char/Walk/Up/images/Char_walk_up_03.png','char/Walk/Up/images/Char_walk_up_04.png','char/Walk/Up/images/Char_walk_up_04.png','char/Walk/Up/images/Char_walk_up_04.png','char/Walk/Up/images/Char_walk_up_04.png','char/Walk/Up/images/Char_walk_up_04.png','char/Walk/Up/images/Char_walk_up_05.png','char/Walk/Up/images/Char_walk_up_05.png','char/Walk/Up/images/Char_walk_up_05.png','char/Walk/Up/images/Char_walk_up_05.png','char/Walk/Up/images/Char_walk_up_05.png','char/Walk/Up/images/Char_walk_up_06.png','char/Walk/Up/images/Char_walk_up_05.png','char/Walk/Up/images/Char_walk_up_06.png','char/Walk/Up/images/Char_walk_up_05.png','char/Walk/Up/images/Char_walk_up_06.png','char/Walk/Up/images/Char_walk_up_05.png','char/Walk/Up/images/Char_walk_up_06.png','char/Walk/Up/images/Char_walk_up_06.png','char/Walk/Up/images/Char_walk_up_06.png']
 
 gameMap = pygame.image.load('map.png')
+gameMap = pygame.transform.scale(gameMap, (display_width * 2, display_height * 2))
 
-
-
-def car(xPos,yPos):
-    gameDisplay.blit(carImg, (xPos,yPos))
-
+mapRect = gameMap.get_rect()
 
 while not crashed:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             crashed = True
+    keys = pygame.key.get_pressed()  #checking pressed keys
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x_change = -speed
-            elif event.key == pygame.K_RIGHT:
-                x_change = speed
-            elif event.key == pygame.K_UP:
-                y_change = -speed
-            elif event.key == pygame.K_DOWN:
-                y_change = speed
-                
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                x_change = 0
-            if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                y_change = 0
+    if keys[pygame.K_UP]:
+        yPos += speed
+        counter = (counter + 1) % len(upImages)
+        playerImg = pygame.image.load(upImages[counter])
+        playerImg = pygame.transform.scale(playerImg, (playerSize,playerSize))
+    if keys[pygame.K_DOWN]:
+        yPos -= speed
+        counter = (counter + 1) % len(downImages)
+        playerImg = pygame.image.load(downImages[counter])
+        playerImg = pygame.transform.scale(playerImg, (playerSize,playerSize))
+    if keys[pygame.K_LEFT]:
+        xPos += speed
+        counter = (counter + 1) % len(leftImages)
+        playerImg = pygame.image.load(leftImages[counter])
+        playerImg = pygame.transform.scale(playerImg, (playerSize,playerSize))
+    if keys[pygame.K_RIGHT]:
+        xPos -= speed
+        counter = (counter + 1) % len(rightImages)
+        playerImg = pygame.image.load(rightImages[counter])
+        playerImg = pygame.transform.scale(playerImg, (playerSize,playerSize))
+
+        # if event.type == pygame.KEYDOWN:
+        #     if event.key == pygame.K_LEFT:
+        #         x_change = speed
+        #     elif event.key == pygame.K_RIGHT:
+        #         x_change = -speed
+        #     elif event.key == pygame.K_UP:
+        #         y_change = speed
+        #     elif event.key == pygame.K_DOWN:
+        #         y_change = -speed
+        #         counter = (counter + 1) % len(downImages)
+        #         playerImg = pygame.image.load(downImages[counter])
 
 
-
-    xPos += x_change
-    yPos += y_change
      
-    gameDisplay.fill(white)
-    gameDisplay.blit(gameMap, (0,0))
-    car(xPos,yPos)
+    gameDisplay.fill(black)
+    gameDisplay.blit(gameMap, (xPos,yPos))
+    gameDisplay.blit(playerImg, (display_width / 2, display_height / 2))
 
-        
     pygame.display.update()
     clock.tick(60)
 
