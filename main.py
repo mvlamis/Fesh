5a2a0f3909d5ee4644cfd5c0537e891f96097623
Michael Vlamis <michael.vlamis@icloud.com>
2022-03-11 13:57:59 -0500

    options menu

--- main.py
+++ main.py
@@ -1,53 +1,145 @@
+from turtle import back
 import pygame
+import time
 
 pygame.init()
 pygame.font.init()
 
+global debug
 debug = False
 
 display_width = 960
 display_height = 544
-
 gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('lil dude man')
-
-speed = 1
 
 black = (0,0,0)
 white = (255,255,255)
 xPos = (-401)
 yPos = (-401)
+speed = 1
+
+font = pygame.font.Font('freesansbold.ttf', 32)
 
 playerSize = 32
 
 counter = 0
 
 clock = pygame.time.Clock()
-crashed = False
+hasStarted = False
 playerImg = pygame.image.load('char/Walk/Down/images/Char_walk_down_01.png')
 playerImg = pygame.transform.scale(playerImg, (playerSize,playerSize))
-
-
-
 
 downImages = ['char/Walk/Down/images/Char_walk_down_01.png','char/Walk/Down/images/Char_walk_down_01.png','char/Walk/Down/images/Char_walk_down_01.png','char/Walk/Down/images/Char_walk_down_01.png','char/Walk/Down/images/Char_walk_down_01.png','char/Walk/Down/images/Char_walk_down_02.png','char/Walk/Down/images/Char_walk_down_02.png','char/Walk/Down/images/Char_walk_down_02.png','char/Walk/Down/images/Char_walk_down_02.png','char/Walk/Down/images/Char_walk_down_02.png','char/Walk/Down/images/Char_walk_down_03.png','char/Walk/Down/images/Char_walk_down_03.png','char/Walk/Down/images/Char_walk_down_03.png','char/Walk/Down/images/Char_walk_down_03.png','char/Walk/Down/images/Char_walk_down_03.png','char/Walk/Down/images/Char_walk_down_04.png','char/Walk/Down/images/Char_walk_down_04.png','char/Walk/Down/images/Char_walk_down_04.png','char/Walk/Down/images/Char_walk_down_04.png','char/Walk/Down/images/Char_walk_down_05.png','char/Walk/Down/images/Char_walk_down_05.png','char/Walk/Down/images/Char_walk_down_05.png','char/Walk/Down/images/Char_walk_down_05.png','char/Walk/Down/images/Char_walk_down_05.png','char/Walk/Down/images/Char_walk_down_06.png','char/Walk/Down/images/Char_walk_down_06.png','char/Walk/Down/images/Char_walk_down_06.png','char/Walk/Down/images/Char_walk_down_06.png','char/Walk/Down/images/Char_walk_down_06.png']
 leftImages = ['char/Walk/Left/images/Char_walk_left_01.png','char/Walk/Left/images/Char_walk_left_01.png','char/Walk/Left/images/Char_walk_left_01.png','char/Walk/Left/images/Char_walk_left_01.png','char/Walk/Left/images/Char_walk_left_01.png','char/Walk/Left/images/Char_walk_left_02.png','char/Walk/Left/images/Char_walk_left_02.png','char/Walk/Left/images/Char_walk_left_02.png','char/Walk/Left/images/Char_walk_left_02.png','char/Walk/Left/images/Char_walk_left_02.png','char/Walk/Left/images/Char_walk_left_03.png','char/Walk/Left/images/Char_walk_left_03.png','char/Walk/Left/images/Char_walk_left_03.png','char/Walk/Left/images/Char_walk_left_03.png','char/Walk/Left/images/Char_walk_left_03.png','char/Walk/Left/images/Char_walk_left_04.png','char/Walk/Left/images/Char_walk_left_04.png','char/Walk/Left/images/Char_walk_left_04.png','char/Walk/Left/images/Char_walk_left_04.png','char/Walk/Left/images/Char_walk_left_05.png','char/Walk/Left/images/Char_walk_left_05.png','char/Walk/Left/images/Char_walk_left_05.png','char/Walk/Left/images/Char_walk_left_05.png','char/Walk/Left/images/Char_walk_left_05.png','char/Walk/Left/images/Char_walk_left_06.png','char/Walk/Left/images/Char_walk_left_06.png','char/Walk/Left/images/Char_walk_left_06.png','char/Walk/Left/images/Char_walk_left_06.png','char/Walk/Left/images/Char_walk_left_06.png']
 rightImages = ['char/Walk/Right/images/Char_walk_right_01.png','char/Walk/Right/images/Char_walk_right_01.png','char/Walk/Right/images/Char_walk_right_01.png','char/Walk/Right/images/Char_walk_right_01.png','char/Walk/Right/images/Char_walk_right_01.png','char/Walk/Right/images/Char_walk_right_02.png','char/Walk/Right/images/Char_walk_right_02.png','char/Walk/Right/images/Char_walk_right_02.png','char/Walk/Right/images/Char_walk_right_02.png','char/Walk/Right/images/Char_walk_right_02.png','char/Walk/Right/images/Char_walk_right_03.png','char/Walk/Right/images/Char_walk_right_03.png','char/Walk/Right/images/Char_walk_right_03.png','char/Walk/Right/images/Char_walk_right_03.png','char/Walk/Right/images/Char_walk_right_03.png','char/Walk/Right/images/Char_walk_right_04.png','char/Walk/Right/images/Char_walk_right_04.png','char/Walk/Right/images/Char_walk_right_04.png','char/Walk/Right/images/Char_walk_right_04.png','char/Walk/Right/images/Char_walk_right_05.png','char/Walk/Right/images/Char_walk_right_05.png','char/Walk/Right/images/Char_walk_right_05.png','char/Walk/Right/images/Char_walk_right_05.png','char/Walk/Right/images/Char_walk_right_05.png','char/Walk/Right/images/Char_walk_right_06.png','char/Walk/Right/images/Char_walk_right_06.png','char/Walk/Right/images/Char_walk_right_06.png','char/Walk/Right/images/Char_walk_right_06.png','char/Walk/Right/images/Char_walk_right_06.png']
 upImages = ['char/Walk/Up/images/Char_walk_up_01.png','char/Walk/Up/images/Char_walk_up_01.png','char/Walk/Up/images/Char_walk_up_01.png','char/Walk/Up/images/Char_walk_up_01.png','char/Walk/Up/images/Char_walk_up_01.png','char/Walk/Up/images/Char_walk_up_02.png','char/Walk/Up/images/Char_walk_up_02.png','char/Walk/Up/images/Char_walk_up_02.png','char/Walk/Up/images/Char_walk_up_02.png','char/Walk/Up/images/Char_walk_up_02.png','char/Walk/Up/images/Char_walk_up_03.png','char/Walk/Up/images/Char_walk_up_03.png','char/Walk/Up/images/Char_walk_up_03.png','char/Walk/Up/images/Char_walk_up_03.png','char/Walk/Up/images/Char_walk_up_03.png','char/Walk/Up/images/Char_walk_up_04.png','char/Walk/Up/images/Char_walk_up_04.png','char/Walk/Up/images/Char_walk_up_04.png','char/Walk/Up/images/Char_walk_up_04.png','char/Walk/Up/images/Char_walk_up_04.png','char/Walk/Up/images/Char_walk_up_05.png','char/Walk/Up/images/Char_walk_up_05.png','char/Walk/Up/images/Char_walk_up_05.png','char/Walk/Up/images/Char_walk_up_05.png','char/Walk/Up/images/Char_walk_up_05.png','char/Walk/Up/images/Char_walk_up_06.png','char/Walk/Up/images/Char_walk_up_06.png','char/Walk/Up/images/Char_walk_up_06.png','char/Walk/Up/images/Char_walk_up_06.png','char/Walk/Up/images/Char_walk_up_06.png']
 
-gameMap = pygame.image.load('map.png')
+cloud1 = pygame.image.load('images/cloud1.png')
+cloud2 = pygame.image.load('images/cloud2.png')
+cloud1 = pygame.transform.scale(cloud1, (37 * 15,21 * 15)).convert()
+cloud2 = pygame.transform.scale(cloud2, (37 * 15,21 * 15)).convert()
+cloud1_x = -600
+cloud2_x = 1200
+
+menuBg = pygame.image.load('images/menubg.png')
+menuBg = menuBg.convert()
+
+startButton = pygame.image.load('images/slay.png')
+optionsButton = pygame.image.load('images/options.png')
+optionsButton = pygame.transform.scale(optionsButton, (100,50))
+global options 
+options = False
+
+gameMap = pygame.image.load('images/map.png')
 gameMap = pygame.transform.scale(gameMap, (display_width * 2, display_height * 2))
+gameMap.set_alpha(0)
+mapAlpha = 0
 
-mapRect = gameMap.get_rect()
+global debugColor
+debugColor = white
 
-while not crashed:
+
+backText = font.render('Back', True, white)
+
+# menu screen
+while not hasStarted:
+    debugText = font.render('Debug mode', True, debugColor)
+    mouse = pygame.mouse.get_pos()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            crashed = True
+            pygame.quit()
+            quit()
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print(mouse[0], mouse[1])
+            # options button
+            if mouse[0] > 440 and mouse[0] < 520 and mouse[1] > 300 and mouse[1] < 370:
+                options = True
+                print("options")
+
+            # start button
+            if mouse[0] > display_width/2 - startButton.get_width()/2 and mouse[1] > display_height/2 - startButton.get_height()/2 and mouse[0] < display_width/2 + startButton.get_width()/2 and mouse[1] < display_height/2 + startButton.get_height()/2:
+                while mapAlpha < 255:
+                    gameDisplay.blit(menuBg, (0,0))
+                    cloud1_x += 7
+                    cloud2_x -= 7
+                    gameDisplay.blit(cloud1, (cloud1_x, 0))
+                    gameDisplay.blit(cloud2, (cloud2_x, 21 * 15))
+                    gameMap.set_alpha(mapAlpha)
+                    mapAlpha += 1
+                    gameDisplay.blit(gameMap, (xPos, yPos))
+                    cloud1.set_alpha(255)
+                    cloud2.set_alpha(255)
+                    pygame.display.update()
+                    clock.tick(60)
+                
+                print("start")
+                hasStarted = True
+
+
+    gameDisplay.fill(white)
+    gameDisplay.blit(menuBg, (0,0))
+    gameDisplay.blit(gameMap, (xPos,yPos))
+
+    if options == True:
+        gameDisplay.blit(menuBg, (0,0))
+        gameDisplay.blit(debugText, (150,150))
+        gameDisplay.blit(backText, (150,450))
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                # debug button
+                if options == True and mouse[0] > 150 and mouse[0] < 350 and mouse[1] > 150 and mouse[1] < 190:
+                    debug = not debug
+                
+                # back button
+                if options == True and mouse[0] > 150 and mouse[0] < 230 and mouse[1] > 450 and mouse[1] < 480:
+                    options = False
+                    
+        if debug:
+            debugColor = (0,255,0)
+        else:
+            debugColor = white
+    else:
+        if hasStarted == False:
+            gameDisplay.blit(startButton, (display_width/2 - startButton.get_width()/2, display_height/2 - startButton.get_height()/2))
+            gameDisplay.blit(optionsButton, (430,330))
+    pygame.display.update()
+    clock.tick(60)
+
+# game loop
+while hasStarted:
+    text = font.render("Position: " + str(xPos) + ", " + str(yPos) + " | Speed: " + str(speed), True, white)
+    debugText = font.render('Debug mode', True, debugColor)
+    mouse = pygame.mouse.get_pos()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            hasStarted = False
     keys = pygame.key.get_pressed()  #checking pressed keys
 
-    if keys[pygame.K_BACKQUOTE]:
-        debug = not debug
+    if keys[pygame.K_ESCAPE]:
+        options = True
+
     if keys[pygame.K_UP]:
         yPos += speed
         counter = (counter + 1) % len(upImages)
@@ -68,12 +160,9 @@ while not crashed:
         counter = (counter + 1) % len(rightImages)
         playerImg = pygame.image.load(rightImages[counter])
         playerImg = pygame.transform.scale(playerImg, (playerSize,playerSize))
-
-
     
     # debug mode
     if debug == True:
-        font = pygame.font.Font('freesansbold.ttf', 18)
         text = font.render("Position: " + str(xPos) + ", " + str(yPos) + " | Speed: " + str(speed), True, white)
         if keys[pygame.K_RIGHTBRACKET]:
             speed += 1
@@ -81,19 +170,19 @@ while not crashed:
             speed -= 1
 
     # left border
-    if xPos == 500:
+    if xPos > 500:
         xPos = xPos - speed
 
     # right border
-    if xPos == -1425:
+    if xPos < -1425:
         xPos = xPos + speed
 
     # top border
-    if yPos == 300:
+    if yPos > 300:
         yPos = yPos - speed
 
     # bottom border
-    if yPos == -785:
+    if yPos < -785:
         yPos = yPos + speed
 
 
@@ -130,18 +219,36 @@ while not crashed:
     if yPos == -160 and xPos < 420 and xPos > 195:
         yPos = yPos - speed
 
-
-    gameDisplay.fill(black)
-    gameDisplay.blit(gameMap, (xPos,yPos))
-    gameDisplay.blit(playerImg, (display_width / 2, display_height / 2))
+    if options == True:
+        clock.tick(60)
+        gameDisplay.blit(menuBg, (0,0))
+        gameDisplay.blit(debugText, (150,150))
+        gameDisplay.blit(backText, (150,450))
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                # debug button
+                if options == True and mouse[0] > 150 and mouse[0] < 350 and mouse[1] > 150 and mouse[1] < 190:
+                    debug = not debug
+                    print('debug')
+                
+                # back button
+                if options == True and mouse[0] > 150 and mouse[0] < 230 and mouse[1] > 450 and mouse[1] < 480:
+                    options = False
+                    
+        if debug:
+            debugColor = (0,255,0)
+        else:
+            debugColor = white
+    else:
+        gameDisplay.fill(black)
+        gameDisplay.blit(gameMap, (xPos,yPos))
+        gameDisplay.blit(playerImg, (display_width / 2, display_height / 2))
     if debug == True:
         gameDisplay.blit(text, (0, 0))
+        
 
     pygame.display.update()
     clock.tick(60)
-
-
-
 
 
 pygame.quit()
